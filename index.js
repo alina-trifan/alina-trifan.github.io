0085c7222ed80d169f0a462e724829d84d9db86d
Alina Trifan <at@staff-1334.wireless.ua.pt>
2021-11-03 13:42:59 +0000

    index

---
+++ index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC2ksOqj6pKo7Qt6n1np-gBY-nuzxl1MKw",
+  authDomain: "research-maps-f0311.firebaseapp.com",
+  projectId: "research-maps-f0311",
+  storageBucket: "research-maps-f0311.appspot.com",
+  messagingSenderId: "722959525813",
+  appId: "1:722959525813:web:20873c094cb2af5878dbfc"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
